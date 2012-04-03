882ece21e85e52b6952517552915d0f434e8bcdc
Justin Taylor <j.taylor8727@gmail.com>
2012-04-02 22:14:24 -0400

    updated to work out of virtualenv and new project structure

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
-LOGFILE=/var/log/gunicorn/example.log
+LOGFILE=/srv/example.com/logs/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3
 # user/group to run as
 USER=root
 GROUP=root
-cd /srv/example.com/current/test_app
-#source ../bin/activate
+
+#DJANGO_SETTINGS_MODULE='production.settings'
+cd /srv/example.com/application/current/project
+
+#use virtualenv
+source ../../../bin/activate
+
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec gunicorn_django -w $NUM_WORKERS \
+exec ../../../bin/gunicorn_django -w $NUM_WORKERS \
 	--user=$USER --group=$GROUP --log-level=debug \
 	--log-file=$LOGFILE 2>>$LOGFILE
