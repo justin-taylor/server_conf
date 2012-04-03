341b5f0a009f02286642b5235a3afdc91c50f296
Justin Taylor <j.taylor8727@gmail.com>
2012-04-03 00:51:16 -0400

    added pid file setting for tracking pid and graceful restart

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+PIDFILE=/srv/example.com/gunicorn.pid
 LOGFILE=/srv/example.com/logs/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3
@@ -16,4 +17,4 @@ source ../../../bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec ../../../bin/gunicorn_django -w $NUM_WORKERS \
 	--user=$USER --group=$GROUP --log-level=debug \
-	--log-file=$LOGFILE 2>>$LOGFILE
+	--log-file=$LOGFILE --pid=$PIDFILE 2>>$LOGFILE
